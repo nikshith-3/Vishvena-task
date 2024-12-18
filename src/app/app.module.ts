c6ca6b8a58d99e2831365b97a0012d9b6c82b889
nikshith-3 <devarasettinikshith9@gmail.com>
2024-12-19 00:07:47 +0530

    vishvena

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ExpertiseComponent } from './expertise/expertise.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ExpertiseComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
